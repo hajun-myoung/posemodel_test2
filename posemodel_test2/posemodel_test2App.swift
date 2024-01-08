9f8df427e199f7202d25d50290b4d6a54065cd67
Denver <audgkwns44@gmail.com>
2024-01-08 16:50:29 +0900

    Initial Commit

---
+++ posemodel_test2/posemodel_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  posemodel_test2App.swift
+//  posemodel_test2
+//
+//  Created by 명하준 on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct posemodel_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
